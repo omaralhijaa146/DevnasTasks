b3fab0b9b94f5bc14ab09dc6f5ce5f215c5a1f06
omar abu alhijaa <omaribraheem222@hotmail.com>
2025-08-14 16:52:00 +0300

    Finished Data TransformerWithFuncAndAction

---
+++ DataTransformerwithFuncandAction/DataTransformer.cs
@@ -0,0 +1,21 @@
+﻿namespace DataTransformerwithFuncandAction;
+
+public class DataTransformer
+{
+    
+    public IEnumerable<int> Transform(IEnumerable<int> numbers,Func<int, int> transfomer)
+    {
+        return numbers.Select(x => transfomer(x));
+    }
+    
+    public void Log(IEnumerable<int> numbers,Action<int> logger)
+    {
+        numbers.ToList().ForEach(x => logger(x));
+    }
+
+    public void ApplyTransformAndLog(IEnumerable<int> numbers,Func<int, int> transfomer, Action<int> logger)
+    {
+        IEnumerable<int> transformedNumbers = Transform(numbers,transfomer).ToList(); 
+        Log(transformedNumbers,logger);
+    }
+}
---
+++ DataTransformerwithFuncandAction/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DataTransformerwithFuncandAction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbersList = Enumerable.Range(1, 10).ToList();
+        var dataTransformer = new DataTransformer();
+        Func<int,int> transformer = x => (int)Math.Pow(x, 2);
+        Action<int> logger = x => Console.WriteLine($"Result = {x}");
+        dataTransformer.ApplyTransformAndLog(numbersList,transformer, 
+            logger);
+        var transformedList = dataTransformer.Transform(numbersList,transformer).ToList();
+        dataTransformer.Log(transformedList,logger);
+    }
+}
+
+/*
+ * Data Transformer with Func and Action
+
+Covered Topics: Delegates & Lambda Expressions
+
+Scenario: A utility must transform raw integer readings, then log the results, all without hard-coding the operations.
+
+Requirements:
+
+Store numbers 1 – 10 in a List<int>.
+
+Declare Func<int,int> transform that squares each value.
+
+Declare Action<int> logger that prints "Result = <value>".
+
+Use Select to apply the Func, then ForEach with the Action.
+
+Verify output shows the square of every original number.
+
+ */
